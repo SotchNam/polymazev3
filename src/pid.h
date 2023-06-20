96562205be6687acf4d79326b2a6bb5884e0ed33
SotchNam <sotchnam@gmail.com>
2023-06-20 03:14:37 +0100

    visually cleanded pid header file

--- src/pid.h
+++ src/pid.h
@@ -1,46 +1,23 @@
-/*************************************************************************
-* PID control system variables 
-*************************************************************************/
 float Kp = 0; //related to the proportional control term; 
-              //change the value by trial-and-error (ex: 0.07).
+//change the value by trial-and-error (ex: 0.07).
 float Ki = 0; //related to the integral control term; 
-              //change the value by trial-and-error (ex: 0.0008).
+//change the value by trial-and-error (ex: 0.0008).
 float Kd = 0; //related to the derivative control term; 
-              //change the value by trial-and-error (ex: 0.6).
+//change the value by trial-and-error (ex: 0.6).
+
 int P;
 int I;
 int D;
-
-/*************************************************************************
-* Global variables
-*************************************************************************/
 int lastError = 0;
 
-/*************************************************************************
-* Motor speed variables (choose between 0 - no speed, and 255 - maximum speed)
-*************************************************************************/
+// Motor speed variables (choose between 0 - no speed, and 255 - maximum speed)
 const uint8_t maxspeeda = 200;
 const uint8_t maxspeedb = 200;
 const uint8_t basespeeda = 120;
 const uint8_t basespeedb = 120;
-/*************************************************************************
-* Function Name: PID_control
-**************************************************************************
-* Summary: 
-* This is the function of the PID control system. The distinguishing 
-* feature of the PID controller is the ability to use the three control 
-* terms of proportional, integral and derivative influence on the controller 
-* output to apply accurate and optimal control. This correction is applied to
-* the speed of the motors, which should be in range of the interval [0, max_speed],
-* max_speed <= 255. 
-* 
-* Parameters:
-* none
-* 
-* Returns:
-*  none
-*************************************************************************/
-void PID_control() {
+
+//calcs corrected motor speeds
+void pidControl() {
   int error = 3500 - position; //3500 is the ideal position (the center)
 
   P = error;
@@ -49,7 +26,6 @@ void PID_control() {
   lastError = error;
 
   int motorspeed = P*Kp + I*Ki + D*Kd; //calculate the correction
-                                       //needed to be applied to the speed
   
   int motorspeeda = basespeeda + motorspeed;
   int motorspeedb = basespeedb - motorspeed;
@@ -67,5 +43,6 @@ void PID_control() {
     motorspeedb = 0;
   } 
   //should be in loop
-  //forward_brake(motorspeeda, motorspeedb);
+  //setMotors(motorspeeda, motorspeedb);
+  //or forward(); idk
 }
